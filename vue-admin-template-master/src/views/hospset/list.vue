d7646d4f431f9cca6eb95d2a4a3ec8dbb34ebb48
Philip <philipj0303@163.com>
2022-07-18 23:08:25 +0800

    添加分页查询以及检索框查询

--- vue-admin-template-master/src/views/hospset/list.vue
+++ vue-admin-template-master/src/views/hospset/list.vue
@@ -2,6 +2,48 @@
   <div class="app-container">
     医院设置列表
 
+    <el-form :inline="true" class="demo-form-inline">
+        <el-form-item>
+            <el-input  v-model="searchObj.hosName" placeholder="医院名称"/>
+        </el-form-item>
+        <el-form-item>
+            <el-input v-model="searchObj.hosCode" placeholder="医院编号"/>
+        </el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+    </el-form>
+
+
+
+    <!-- banner列表 -->
+    <el-table
+    :data="list"
+    stripe
+        style="width: 100%">
+
+    <el-table-column type = "index" width = "50" label = "序号"/>
+    <el-table-column prop = "hosName" label = "医院名称"/>
+    <el-table-column prop = "hosCode" label = "医院编号"/>
+    <el-table-column prop = "apiUrl" label = "api基础路径" width = "200"/>
+    <el-table-column prop = "contactsName" label = "联系人姓名"/>
+    <el-table-column prop = "contactsPhone" label = "联系人手机"/>
+    <el-table-column label = "状态" width = "80">
+    <template slot-scope="scope">
+            {{ scope.row.status === 1 ? '可用' : '不可用' }}
+    </template>
+    </el-table-column>
+    </el-table>
+
+    <!-- 分页 -->
+    <el-pagination
+    :current-page="current"
+    :page-size="limit"
+    :total="total"
+    style="padding: 30px 0; text-align: center;"
+    layout="total, prev, pager, next, jumper"
+    @current-change="getList"
+    />
+
+
   </div>
 </template>
 
@@ -32,7 +74,8 @@ export default{
     methods:{
         //定义方法进行请求接口调用
         //医院设置列表
-        getList(){
+        getList(page = 1){   //添加当前页参数
+            this.current = page
             hospset.getHospSetList(this.current, this.limit, this.searchObj)
             .then(response => {
                 //返回集合赋值给list
